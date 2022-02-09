01e921f18729ff075e8f81aff2ead6124a810a8c
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-09 21:20:27 +0800

    Add Button

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.awt.Button;
 import java.awt.Frame;
 
 public class Main extends Frame {
 
 	Main() {
+		Button addButton = new Button("Add");
+		addButton.setBounds(0, 330, 80, 30);
+		add(addButton);
+
 		setTitle("Advanced Calculator");
 		setSize(480, 360);
+		setLayout(null);
 		setVisible(true);
 	}
 
