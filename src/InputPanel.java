97f82a63ea2e76b3a5c9e16f155b1bc9cb085048
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-09 22:23:49 +0800

    Input Value with Spacing

--- src/InputPanel.java
+++ src/InputPanel.java
@@ -1,10 +1,14 @@
 import java.awt.Choice;
 import java.awt.Component;
 import java.awt.Panel;
+import java.awt.TextField;
 
 public class InputPanel {
 	public static Component GetInputPanel() {
 		Panel p = new Panel();
+
+		TextField num1 = new TextField(5);
+		p.add(num1);
 
 		Choice math = new Choice();
 		math.add("+");
@@ -12,6 +16,9 @@ public class InputPanel {
 		math.add("x");
 		math.add("÷");
 		p.add(math);
+
+		TextField num2 = new TextField(5);
+		p.add(num2);
 
 		p.setBounds(0, 30, 480, 30);
 		return p;
