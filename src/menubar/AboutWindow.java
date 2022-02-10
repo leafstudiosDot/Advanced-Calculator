745b93406ce4aebcb0386c63c375b5c672b5e46e
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-10 10:50:30 +0800

    About Menu Event

---
+++ src/menubar/AboutWindow.java
@@ -0,0 +1,13 @@
+package menubar;
+
+import java.awt.Frame;
+
+public class AboutWindow extends Frame {
+	AboutWindow() {
+		setTitle("Advanced Calculator");
+		setSize(480, 360);
+		setResizable(false);
+		setLayout(null);
+		setVisible(true);
+	}
+}
--- src/menubar/MenuBarC.java
+++ src/menubar/MenuBarC.java
@@ -3,14 +3,31 @@ package menubar;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuBarC {
 	public static MenuBar GetMenuBar() {
 		MenuBar menubar = new MenuBar();
+
 		Menu helpMB = new Menu("Help");
+
 		MenuItem aboutMBI = new MenuItem("About");
+		aboutMBI.setActionCommand("About Software");
 		helpMB.add(aboutMBI);
+
 		menubar.add(helpMB);
+
+		MenuItemListener menuItemListener = new MenuItemListener();
+		aboutMBI.addActionListener(menuItemListener);
+
 		return menubar;
+	}
+}
+
+class MenuItemListener implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Menu pressed: " + e.getActionCommand());
 	}
 }
