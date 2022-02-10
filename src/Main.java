03deca6787dd92b87f412ddcde529c78b32100e2
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-10 08:49:19 +0800

    Close Window

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.awt.Button;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Main extends Frame {
 	static int inpCount = 1;
@@ -31,6 +33,13 @@ public class Main extends Frame {
 		setResizable(false);
 		setLayout(null);
 		setVisible(true);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+			}
+		});
 	}
 
 	public static void main(String[] args) {
