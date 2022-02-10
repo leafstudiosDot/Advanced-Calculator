9ca51335a7d988f5411b60d0be322c8942506750
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-02-10 08:44:40 +0800

    ReadOnly Input

--- src/InputPanel.java
+++ src/InputPanel.java
@@ -1,11 +1,13 @@
 import java.awt.Choice;
 import java.awt.Component;
+import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextField;
 
 public class InputPanel {
+	private static Panel p = new Panel();
+
 	public static Component GetInputPanel(int y) {
-		Panel p = new Panel();
 
 		TextField num1 = new TextField(5);
 		p.add(num1);
@@ -20,7 +22,18 @@ public class InputPanel {
 		TextField num2 = new TextField(5);
 		p.add(num2);
 
+		Label equals = new Label("=");
+		p.add(equals);
+
+		TextField num3 = new TextField(5);
+		num3.setEditable(false);
+		p.add(num3);
+
 		p.setBounds(0, y, 480, 30);
 		return p;
+	}
+
+	public void DeleteRow() {
+		p.setVisible(false);
 	}
 }
